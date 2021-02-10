e3809ea0324a44ca74384a2571fa2277e1ed8266
Ildar Bikmamatov <ildar@bayrell.org>
2021-02-10 18:40:10 +0000

    add arm64

--- build.sh
+++ build.sh
@@ -25,31 +25,40 @@ case "$1" in
 			--file Dockerfile --build-arg ARCH=arm32v7/
 	;;
 	
+	arm64v8)
+		docker build ./ -t bayrell/alpine:$VERSION-$SUBVERSION-arm64v8 \
+			--file Dockerfile --build-arg ARCH=arm64v8/
+	;;
+	
 	manifest)
 		rm -rf ~/.docker/manifests/docker.io_bayrell_alpine-*
 		
 		docker push bayrell/alpine:$VERSION-$SUBVERSION-amd64
 		docker push bayrell/alpine:$VERSION-$SUBVERSION-arm32v7
+		docker push bayrell/alpine:$VERSION-$SUBVERSION-arm64v8
 		
 		docker manifest create bayrell/alpine:$VERSION-$SUBVERSION \
 			--amend bayrell/alpine:$VERSION-$SUBVERSION-amd64 \
-			--amend bayrell/alpine:$VERSION-$SUBVERSION-arm32v7
+			--amend bayrell/alpine:$VERSION-$SUBVERSION-arm32v7 \
+			--amend bayrell/alpine:$VERSION-$SUBVERSION-arm64v8
 		docker manifest push bayrell/alpine:$VERSION-$SUBVERSION
 		
 		docker manifest create bayrell/alpine:$VERSION \
 			--amend bayrell/alpine:$VERSION-$SUBVERSION-amd64 \
-			--amend bayrell/alpine:$VERSION-$SUBVERSION-arm32v7
+			--amend bayrell/alpine:$VERSION-$SUBVERSION-arm32v7 \
+			--amend bayrell/alpine:$VERSION-$SUBVERSION-arm64v8
 		docker manifest push bayrell/alpine:$VERSION
 	;;
 	
 	all)
 		$0 amd64
+		$0 arm64v8
 		$0 arm32v7
 		$0 manifest
 	;;
 	
 	*)
-		echo "Usage: $0 {amd64|arm32v7|manifest|all|test}"
+		echo "Usage: $0 {amd64|arm64v8|arm32v7|manifest|all|test}"
 		RETVAL=1
 
 esac
